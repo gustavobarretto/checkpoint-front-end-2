935b12a8ca92772e2d86a61dd8e0e84b4423cf88
Gustavo Barretto <gustavobarretto@gmail.com>
2021-09-03 18:07:40 -0300

    adicionando js inicial e ajustes de estilização

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+const enviarInfo = () => {
+    
+    const categoria = document.createElement('h2')
+    categoria.textContent = document.getElementById('option').value
+    document.querySelector('[class=imagem]').appendChild(categoria)
+
+    const autor = document.createElement('h3')
+    autor.textContent = document.getElementById('autor').value
+    document.querySelector('[class=imagem]').appendChild(autor)
+    
+    const titulo = document.createElement('h3')
+    titulo.textContent = document.getElementById('titulo').value
+    document.querySelector('[class=imagem]').appendChild(titulo)
+    
+    const link = document.createElement('a')
+    link.setAttribute('href', document.getElementById('link').value)
+    link.textContent = 'Link da Receita'
+    document.querySelector('[class=imagem]').appendChild(link)
+
+    const texto = document.createElement('p')
+    texto.textContent = document.getElementById('texto').value
+    document.querySelector('[class=imagem]').appendChild(texto)
+
+
+}
+
+document.querySelector('[class=submit]').addEventListener('click', enviarInfo)
